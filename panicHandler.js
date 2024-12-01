66f40c13305f38fd6a7eef7fa2df8cd1384f35ba
Luís Augusto Sousa <83593485+D1p7@users.noreply.github.com>
2024-12-01 20:51:51 -0300

    Update panicHandler.js

--- panicHandler.js
+++ panicHandler.js
@@ -10,7 +10,7 @@ export const handlePanic = async () => {
     }
 
     const irlLocation = async () => {
-        const location = await fetch('link');
+        const location = await fetch('https://realtime-location-api.onrender.com/localizacao/current');
         const data = await location.json();
         return data.message;
     };
@@ -20,7 +20,7 @@ export const handlePanic = async () => {
     contatos.forEach(async (contato) => {
         try {
             console.log(`Enviando mensagem para o número: ${contato.numeroComDdd}`);
-            const response = await axios.post('link', {
+            const response = await axios.post('https://2bf1-2804-7d74-82-f900-712e-41ac-220b-2285.ngrok-free.app/message/sendText/Alerta', {
                 number: contato.numeroComDdd,
                 textMessage: {
                     text: `Mensagem de teste, Localização: ${message}`
@@ -32,7 +32,7 @@ export const handlePanic = async () => {
                 }
             }, {
                 headers: {
-                    'apikey': 'key'
+                    'apikey': 'B6D711FCDE4D4FD5936544120E713976'
                 }
             });
             console.log(`Mensagem enviada para ${contato.celular}:`, response.data);
